5a3136da3c20a84531cb627169685bd395f4d669
MarselMingaripov <bard1812@gmail.com>
2022-11-29 19:29:59 +0300

    выполнил задание 7

--- src/Main.java
+++ src/Main.java
@@ -23,6 +23,12 @@ public class Main {
             System.out.println("Есть стоячие места");
         else System.out.println("Есть сидячие места");
 
-
+        if (age >= 2 && age <= 6)
+            System.out.println("Если возраст человека равен " + age + ", то ему нужно ходить в детский сад");
+        else if (age >= 7  && age <= 18)
+            System.out.println("Если возраст человека равен " + age + ", то ему нужно ходить в школу");
+        else if (age >= 19 && age <= 23)
+            System.out.println("Если возраст человека равен " + age + ", то ему нужно ходить в университет");
+        else System.out.println("Если возраст человека равен " + age + ", то ему нужно ходить на работу");
     }
 }
