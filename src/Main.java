0d3db125b98b69708d4e0fddf80efaea5d8417f8
MarselMingaripov <bard1812@gmail.com>
2022-11-29 19:17:18 +0300

    выполнил задание 4,5,6

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,18 @@
 public class Main {
     public static void main(String[] args) {
 
-        int age = 10;
+        int age = 18;
         if (age >= 18)
             System.out.println("Поздравляю с совершеннолетием!");
         else System.out.println("Вы еще не достигли совершеннолетия!");
 
-        if(age >= 7)
-            System.out.println("Ребенок ходит в школу");
+        if(age >= 24)
+            System.out.println("Человек окончил университет и ему пора искать первую работу");
+        else
         if (age >= 18)
             System.out.println("Человек уже закончил школу и может отправляться в университет");
-        if (age >= 24)
-            System.out.println("Человек окончил университет и ему пора искать первую работу");
+        else if (age >= 7)
+            System.out.println("Ребенок ходит в школу");
 
         int totalCapasity = 102;
         int seatCapasity = 60;
@@ -21,5 +22,7 @@ public class Main {
         else if (countOfPassengers > seatCapasity)
             System.out.println("Есть стоячие места");
         else System.out.println("Есть сидячие места");
+
+
     }
 }
